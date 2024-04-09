0fe54a3f30e5fb385ddae92a95ff232b6b815696
Chakradhar Dwivedi <chakradhar.dwivedi2000@gmail.com>
2024-04-09 15:52:41 +0530

    Added map gen and green columns

---
+++ colours/colours.go
@@ -0,0 +1,10 @@
+
+package colors
+
+import 	"github.com/fatih/color"
+
+ 
+var Green = color.New(color.BgGreen)
+var Red = color.New(color.BgRed)
+var Yellow = color.New(color.BgYellow)
+var Blue = color.New(color.BgBlue)
--- main.go
+++ main.go
@@ -3,26 +3,47 @@ package main
 import (
 	"fmt"
 	"jumpingGame/structs"
+	"jumpingGame/colours"
+	"jumpingGame/terminal"
+	"math/rand"
 )
 
 var grid [30][100]structs.Cell
+var nextObsStart int
 
-func main(){
-	for r:= range grid {
-		for _,val := range grid[r] {
-			if val.IsVisible == false{
-				fmt.Print("*")
+func main() {
+	terminal.CallClear()
+	genMap()
+	for r := range grid {
+		for _, val := range grid[r] {
+			if val.IsVisible {
+				colors.Green.Print(" ")	
+			} else {
+				fmt.Print(" ")
 			}
 		}
 		fmt.Println()
 	}
 }
 
-func genMap(){
-	for r:= range grid {
-		for c,val := range grid[r]{
-			genRandomNumber 
-		}
+func genMap() {
+	for i := 20; i < 100; i++ {
+		nextObsStart = i
+		hTop := rand.Intn(20)
+		genObs(hTop)
+		i = i + 20
 	}
 }
 
+func genObs(heigth int) {
+	for k := 0; k <= heigth; k++ {
+		for i := nextObsStart; i <= nextObsStart+6 && i < 100; i++ {
+			grid[k][i].IsVisible = true
+		}
+	}
+	for k:=heigth + 6;k<30;k++{
+		for h:=nextObsStart;h<=nextObsStart+6 && h<100;h++{
+			grid[k][h].IsVisible = true
+		}
+	}
+}
