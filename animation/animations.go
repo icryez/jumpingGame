791cf8324283f19859bb0696f863092218d4f1d9
Chakradhar Dwivedi <chakradhar.dwivedi2000@gmail.com>
2024-04-11 12:45:43 +0530

    Refactor animation code

--- animation/animations.go
+++ animation/animations.go
@@ -13,17 +13,11 @@ var gameOver bool
 var FgridCurrCol int
 func Tick() {
 	gameOver = false
-	FgridCurrCol = 0
 	mapModule.GenFgrid()
 	for gameOver == false {
 		
-		time.Sleep(150 * time.Millisecond)
-		animateToLeft()
+		time.Sleep(100* time.Millisecond)
 		printGrid(mapModule.MainGrid)
-		if FgridCurrCol == 100 {
-			FgridCurrCol = 0
-			mapModule.GenFgrid()
-		}
 	}
 
 }
@@ -43,14 +37,15 @@ func printGrid(grid [30][100]structs.Cell) {
 	}
 }
 
-func animateToLeft() {
+
+func AnimateToLeft() {
 	for i := 0; i < 30; i++ {
 		for j := 0; j < 99; j++ {
 			mapModule.MainGrid[i][j] = mapModule.MainGrid[i][j+1]
 		}
 	}
-	for i:=0;i<30;i++{
-		mapModule.MainGrid[i][99]=mapModule.Fgrid[i][FgridCurrCol]
+	for i := 0; i < 30; i++ {
+		mapModule.MainGrid[i][99] = mapModule.Fgrid[i][FgridCurrCol]
 	}
 	FgridCurrCol++
 }
--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"jumpingGame/animation"
+	"jumpingGame/mapModule"
+	"time"
 )
 
 
 func main() {
-	go animation.Tick();
+	go animation.Tick()
+	animation.FgridCurrCol = 0
 	for true {
+		time.Sleep(200* time.Millisecond)
+		animation.AnimateToLeft()
+		if animation.FgridCurrCol == 100 {
+		animation.FgridCurrCol = 0
+			mapModule.GenFgrid()
+		}
 	}
 }
 
